061ac7b76c84ad55c684d9e5edeff28562e4fc0d
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-20 13:31:23 +0100

    Outputs url HTML

--- lib/ruby_web_scraper.rb
+++ lib/ruby_web_scraper.rb
@@ -2,4 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 
 url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
-puts document = open(url)
+document = open(url)
+content = document.read
+
+puts content
